9c7879d814f41e5d69fbd18781b5b2062ff0c409
yeoto <yeoto867@naver.com>
2019-05-01 15:32:41 +0900

    add List Control

---
+++ HeaderStone_Deck_Maker/CardListCtrl.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CardListCtrl.h"
+
+
+CCardListCtrl::CCardListCtrl()
+{
+}
+
+
+CCardListCtrl::~CCardListCtrl()
+{
+}
---
+++ HeaderStone_Deck_Maker/CardListCtrl.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <afxwin.h>
+class CCardListCtrl :
+	public CWnd
+{
+public:
+	CCardListCtrl();
+	virtual ~CCardListCtrl();
+};
+
