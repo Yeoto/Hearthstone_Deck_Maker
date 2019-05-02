461c8fc0ca8c75d667571d310fcdfa4145c18fa2
yeoto <yeoto867@naver.com>
2019-05-02 22:43:02 +0900

    add Card Notifier / redesign algorithm for card page

---
+++ HeaderStone_Deck_Maker/CardNotifier.h
@@ -0,0 +1,15 @@
+#if !defined(__CARDNOTIFIER_H__)
+#define __CARDNOTIFIER_H__
+
+#pragma once
+
+#include <vector>
+
+class CCardNotifier
+{
+public:
+	CCardNotifier();
+	~CCardNotifier();
+};
+
+#endif
